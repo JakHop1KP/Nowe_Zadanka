dc7474b9961fbe5c132213271a6b23816c6445d5
JakHop1KP <ligmastysigma@gmail.com>
2026-09-15 15:48:10 +0200

    Add files via upload

---
+++ Hoppe_zewnetrzny.js
@@ -0,0 +1,6 @@
+document.write("Mój numer w dzienniku to 5"+"<br>");
+document.write("Pierwszy wiersz to: &nbsp;12&nbsp; Nie&nbsp; Tak&nbsp; 7"+"<br>");
+document.write("Drugi wiersz to: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;12&nbsp; Nie&nbsp; Tak&nbsp; 7"+"<br>");
+document.write("<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("yellow").strike().fontsize(7)+"<br>"); 
+document.write(document.lastModified.fontcolor("yellow").strike().fontsize(7));
